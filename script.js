d23e42b6554e2dd7f80e6b911ca9a712667ac960
Tinashe Magazeni <magazenit@gmail.com>
2024-08-29 06:42:19 +0300

    functions to enter and check the word guessed and some styling

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
  const buttonElements = document.querySelectorAll('button');
 let row = 1;
 let letter = 1;
+let count = 0;
 const wordElements = document.querySelectorAll('.row');
+const wordForTheDay = 'mouth';
 
 buttonElements.forEach((element) => {
     element.addEventListener('click', function () {
         keypress(element.attributes["data-key"].value)
     });
-
 });
 
 function populateword(key){
     if (letter < 6){
         wordElements[row-1].querySelectorAll('.word')[letter-1].innerText= key;
         letter += 1;
-
     }
 }
 
-/*function enterword(){
+function checkWord(){
+    const letterElements = wordElements[row-1].querySelectorAll('.word');
+
+    letterElements.forEach((element, index) => {
+        const indexOfLetterInWordOfTheDay = wordForTheDay.toLowerCase().indexOf
+        (element.innerText.toLowerCase());
+
+        //console.log(indexOfLetterInWordOfTheDay);
+        if (indexOfLetterInWordOfTheDay === index ) {
+            element.classList.add('word-green');
+        } else if (indexOfLetterInWordOfTheDay >= 0) {
+            element.classList.add('word-yellow');
+        } else {
+            element.classList.add('word-grey');
+        }
+    });
+}
+
+function enterWord(){
     if (letter < 6){
-        //alert('You have to have letters for each spot')
-    } else {
-        c
+        alert('You have to have letters for each spot');
+  } else {
+        checkWord();
+        row += 1;
+        letter = 1;
+       // count +=1;
+        //console.log(count);
     }
-}*/
+}
 function keypress(key) {
 if (key.toLowerCase() === 'enter'){
-   //enterword();
+   enterWord();
 } else if (key.toLowerCase() === 'del' ){
     //deleteletter();
 }else{
